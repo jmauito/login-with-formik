004dfc7ce563107f23e8664d32768c3e92c35f3c
jmauito <jmauito@gmail.com>
2023-05-02 18:16:39 -0500

    Added email validation

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ function App() {
     
     onSubmit: values => {
       console.log('Form: ', values);
+      alert("Login Successful");
     },
 
     validate: values => {
       const errors = {};
-      if(!values.name) errors.name = 'Username is required';
-      if(!values.password) errors.password = 'Password is required';
+      if(!values.name) errors.name = 'Field required';
+      if( !values.name.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) ) errors.name = 'Not is a valid email'
+      if(!values.password) errors.password = 'Field required';
       return errors;
     }
 
@@ -24,12 +26,12 @@ function App() {
     <div className="App">
       <form onSubmit={formik.handleSubmit}>
         <div>Username</div>
-        <input type="text" name="name" onChange={formik.handleChange} value={formik.values.name} />
-        { formik.errors.name ? <div style={{color:'red'}}>{formik.errors.name}</div> : null }
+        <input id="emailField" type="email" name="name" onChange={formik.handleChange} value={formik.values.name} />
+        { formik.errors.name ? <div id="emailError" style={{color:'red'}}>{formik.errors.name}</div> : null }
         <div>Password</div>
-        <input type="password" name="password" onChange={formik.handleChange} value={formik.values.password} />
-        { formik.errors.password ? <div style={{color:'red'}}>{formik.errors.password}</div> : null }
-        <button type="submit">Login</button>
+        <input id="pswField" type="password" name="password" onChange={formik.handleChange} value={formik.values.password} />
+        { formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div> : null }
+        <button id="submitBtn" type="submit">Login</button>
       </form>
     </div>
   );
